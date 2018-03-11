2a67df2592070ee866e5b583c8bf18c13453ffae
Mohammed Noori <kmanoorib@gmail.com>
2018-03-11 03:43:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,12 +26,12 @@ function currentLine (katzDeli){
 var i = 0;
 
 while (i < katzDeli.length) {
-  theline.push(` `+[i+1]+`. `  + katzDeli[i])
+  line.push(` `+[i+1]+`. `  + katzDeli[i])
   i++;
 }
 
 if (katzDeli.length === 0) {
   return ('The line is currently empty.');
 }
-else return ('The line is currently: ' + theline);
+else return ('The line is currently: ' + line);
 }
