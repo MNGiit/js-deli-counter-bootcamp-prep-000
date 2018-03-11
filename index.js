e179895c599a19f9556f50e40180da28a43f9053
Mohammed Noori <kmanoorib@gmail.com>
2018-03-11 02:39:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var katzDeli = [];
 
 function takeANumber(katzDeliLine, name){
+  var num;
   
   katzDeliLine.push(name);
-  
+  num = katzDeliLine.find(name);
+  num++;
   return katzDeliLine;
 }
