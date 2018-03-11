8df6009dbdae716073379604bd33448bc2dc6075
Mohammed Noori <kmanoorib@gmail.com>
2018-03-11 02:52:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function takeANumber(katzDeli, name){
   var num;
   
   katzDeli.push(`${name}`);
-  num = katzDeli.find('${name}');
+  num = katzDeli.find(`${name}`);
   num++;
   return ('Welcome, ${name}. You are number ${num} in line.');
 }
