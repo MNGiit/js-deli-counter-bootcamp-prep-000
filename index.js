181d9a8df62f88ffa5e701b5b014d23eb01c1a6c
Mohammed Noori <kmanoorib@gmail.com>
2018-03-11 03:46:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,14 +19,14 @@ function nowServing (katzDeli){
 
 }
 
-var line = [];
+var theline = [];
 
 function currentLine (katzDeli){
 
 var i = 0;
 
 while (i < katzDeli.length) {
-  line.push(` `+[i+1]+`. `  + katzDeli[i])
+  theline.push(` `+[i+1]+`. `  + katzDeli[i])
   i++;
 }
 
@@ -34,6 +34,6 @@ if (katzDeli.length === 0) {
   return ('The line is currently empty.');
 }
 else {
-  return (`The line is currently:` + line);
+  return (`The line is currently:` + theline);
 }
 }
